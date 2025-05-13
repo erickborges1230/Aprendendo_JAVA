73b612052916532cc5a704f94ea0a79ab94562a1
Erick Borges <erickborges1230@gmail.com>
2025-05-13 14:21:32 -0300

    fourth commit

---
+++ src/main/java/org/example/Jantar.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Jantar {
+    public static void main(String[] args) {
+        final double taxa_garcom = 0.1; //constante
+        double valor_jantar, valor_jantar_total; //variáveis
+        valor_jantar = Double.parseDouble(JOptionPane.showInputDialog("Informe o valor do jantar"));
+        valor_jantar_total = valor_jantar + (taxa_garcom * valor_jantar);
+        JOptionPane.showMessageDialog(null, "O valor total do jantar a ser pago é de "+valor_jantar_total);
+    }
+}
