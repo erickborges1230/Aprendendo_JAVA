4ab70317fc6b999b855ad456f5ef9262ccb5bf94
Erick Borges <erickborges1230@gmail.com>
2025-05-13 14:08:23 -0300

    third commit

---
+++ src/main/java/org/example/CalcularDobro.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class CalcularDobro {
+    public static void main(String[] args){
+        //Versão 1 do código
+        /*
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o seu número");
+        double numero = teclado.nextDouble();
+        double dobro = numero * 2;
+        System.out.println("O dobro do número é "+dobro);
+
+         */
+        //=> Versão 2 do código
+        var numero = JOptionPane.showInputDialog("Digite o seu número");
+        //Será preciso fazer uma conversão
+        //Convertendo uma String para um tipo REAL
+        var dobro = Double.parseDouble(numero) * 2;
+        JOptionPane.showMessageDialog(null, "O dobro do número é"+dobro);
+    }
+}
