3906024ae666b0c738f42b79acf9da27433d3ccf
Erick Borges <erickborges1230@gmail.com>
2025-05-13 14:53:58 -0300

    first commit

---
+++ src/main/java/org/example/Numero.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Numero {
+    public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("informe o número"));
+        int anterior = numero - 1;
+        int posterior = numero + 1;
+        JOptionPane.showMessageDialog(null,"O valor do número anterior = "+anterior+" e o posterior = "+posterior);
+    }
+}
