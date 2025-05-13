8cab1ee541aadd2b2a39b53e03dd870f1d67b81b
Erick Borges <erickborges1230@gmail.com>
2025-05-13 14:41:18 -0300

    first commit

---
+++ src/main/java/org/example/CalcularHoras.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+import java.util.Optional;
+
+public class CalcularHoras {
+    public static void main(String[] args) {
+        int dias = Integer.parseInt(JOptionPane.showInputDialog("Informar o número de dias de viagem"));
+        int horas = Integer.parseInt(JOptionPane.showInputDialog("informar o número de horas de viagem"));
+        int total_horas = (dias*24) + horas;
+        JOptionPane.showMessageDialog(null,"O valor da viagem em horas foi de "+total_horas);
+
+    }
+}
