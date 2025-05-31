b0617fb2bd4a7798878820fdd6c4676c954a83ac
Erick Borges <erickborges1230@gmail.com>
2025-05-31 14:12:29 -0300

    first commit

--- src/main/java/org/example/Equacao2Grau.java
+++ src/main/java/org/example/Equacao2Grau.java
@@ -8,18 +8,18 @@ public class Equacao2Grau {
         int a, b, c;
         double delta, x1, x2;
         Scanner teclado = new Scanner(System.in);
-        System.out.println("Digite o valor de A");
+        System.out.println("Digite o valor de A ");
         a = teclado.nextInt();
-        System.out.println("Digite o valor de B");
+        System.out.println("Digite o valor de B ");
         b = teclado.nextInt();
-        System.out.println("Digite o valor de C");
+        System.out.println("Digite o valor de C ");
         c = teclado.nextInt();
 
         delta = b*b - 4*a*c;
         if(delta >=0) //existem caízes rais
         {
-            x1 = -b - (Math.sqrt(delta))/2*a;
-            x2 = -b + (Math.sqrt(delta))/2*a;
+            x1 = (-b - (Math.sqrt(delta)))/2*a;
+            x2 = (-b + (Math.sqrt(delta)))/2*a;
             System.out.println("O valor de X1 "+x1);
             System.out.println("O valor de X2 "+x2);
 
